024b029a2bd64e48d7f3b79abad615b644313da4
Louise <louisehomann@gmail.com>
2019-03-06 14:25:59 +0100

    header

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>JL Idrætsforening</title>
+</head>
+
+<body>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+</body>
+
+</html>
+</body>
+
+</html>
